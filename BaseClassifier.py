fixed normalization issue

BaseClassifier.py
@@ -68,27 +68,19 @@ class RangeNormalizer(DataNormalizer):
 
 class BaseClassifier:
 
-    def __init__(self, epsilon, inter_cell_bound=100, normalizer_mode=None, algorithm_mode=None,
-                 feature_drop_probability=0.0):
+    def __init__(self, epsilon, inter_cell_bound=100, algorithm_mode=None):
         # hyper parameters
         self.epsilon = epsilon
         self.interCellBound = inter_cell_bound
         self.cell_step = epsilon / inter_cell_bound
-        if normalizer_mode is None or normalizer_mode == "range":
-            self.normalizer = RangeNormalizer()
-        else:
-            self.normalizer = NormOneNormalizer()
         self.algorithm_mode = algorithm_mode  # if None, to be inferred when the data is available
-        self.feature_drop_probability = feature_drop_probability
 
         # data related
         self.data = None
-        self.features_to_keep = None
         self.data_dist = None
         self.labels = None
 
         # internal values for classifiers
-        self.data_normalized = None
         self.a = None
         self.b = None
         self.m = -1.0
@@ -101,10 +93,10 @@ class BaseClassifier:
     def set_globals(self):
         weighted_label = np.multiply(self.data_dist, self.labels)
         self.m = weighted_label.sum()
-        self.a = np.squeeze(np.dot(self.data_dist.transpose(), self.data_normalized))
+        self.a = np.squeeze(np.dot(self.data_dist.transpose(), self.data))
         if len(self.a.shape) == 0:
             self.a = np.reshape(self.a, (-1))
-        self.b = np.squeeze(np.dot(weighted_label.transpose(), self.data_normalized))
+        self.b = np.squeeze(np.dot(weighted_label.transpose(), self.data))
         if len(self.b.shape) == 0:
             self.b = np.reshape(self.b, (-1))
 
@@ -290,17 +282,9 @@ class BaseClassifier:
         self.g = best[0]
 
     def approximate_solver(self, data, labels, data_dist):
-        self.features_to_keep = np.zeros((0,))
-        while np.equal(self.features_to_keep.sum(), 0.0):
-            self.features_to_keep = np.greater_equal(
-                np.random.uniform(0.0, 1.0, data.shape[1]), self.feature_drop_probability)
-
         self.data = data
         self.labels = labels
         self.data_dist = data_dist
-        # self.data_normalized = self.normalizer.normalize_train(data)
-        self.data_normalized = data[:, self.features_to_keep]
-        self.data_normalized = self.normalizer.normalize_train(self.data_normalized)
         self.set_globals()
         self.set_dependency_coefficient()
         if np.equal(0.0, self.lambda_):
@@ -319,28 +303,27 @@ class BaseClassifier:
             self.approximate_dependent()
         # self.set_w_to_positive_side()
 
-    def set_w_to_positive_side(self):
-        probabilities_positive = self.get_probabilities(self.data)
-        probabilities_negative = np.subtract(1.0, probabilities_positive)
-
-        positive_labels = np.dot(probabilities_positive, self.labels)
-        negative_labels = np.dot(probabilities_negative, self.labels)
-
-        if np.greater_equal(positive_labels, negative_labels):
-            return
-
-        self.w = np.multiply(-1.0, self.w)
-        probabilities_positive = self.get_probabilities(self.data)
-        probabilities_negative = np.subtract(1.0, probabilities_positive)
-
-        positive_labels = np.dot(probabilities_positive, self.labels)
-        negative_labels = np.dot(probabilities_negative, self.labels)
-        if np.less(positive_labels, negative_labels):
-            raise Exception('Invalid situation')
+    # def set_w_to_positive_side(self):
+    #     probabilities_positive = self.get_probabilities(self.data)
+    #     probabilities_negative = np.subtract(1.0, probabilities_positive)
+    #
+    #     positive_labels = np.dot(probabilities_positive, self.labels)
+    #     negative_labels = np.dot(probabilities_negative, self.labels)
+    #
+    #     if np.greater_equal(positive_labels, negative_labels):
+    #         return
+    #
+    #     self.w = np.multiply(-1.0, self.w)
+    #     probabilities_positive = self.get_probabilities(self.data)
+    #     probabilities_negative = np.subtract(1.0, probabilities_positive)
+    #
+    #     positive_labels = np.dot(probabilities_positive, self.labels)
+    #     negative_labels = np.dot(probabilities_negative, self.labels)
+    #     if np.less(positive_labels, negative_labels):
+    #         raise Exception('Invalid situation')
 
     def get_probabilities(self, data):
-        normalized_data = self.normalizer.normalize_test(data[:, self.features_to_keep])
-        res = np.multiply(np.add(np.dot(normalized_data, self.w), 1.0), 0.5)
+        res = np.multiply(np.add(np.dot(data, self.w), 1.0), 0.5)
         # make sure probabilities are in range
         res[np.less(res, 0.0)] = 0.0
         res[np.greater(res, 1.0)] = 1.0

TreeClassifier.py
@@ -6,14 +6,12 @@ import numpy as np
 
 class TreeNode:
 
-    def __init__(self, data, data_dist, labels, epsilon, normalizer_mode, feature_drop_probability):
+    def __init__(self, data, data_dist, labels, epsilon):
         # data
         self.data = data
         self.data_dist = data_dist
         self.labels = labels
         self.epsilon = epsilon
-        self.normalizer_mode = normalizer_mode
-        self.feature_drop_probability = feature_drop_probability
 
         # internal data structures
         self.left = None
@@ -33,10 +31,7 @@ class TreeNode:
         normalized_data_dist = np.divide(self.data_dist, self.weight)
         normalized_epsilon = self.epsilon / self.weight
         normalized_epsilon = np.min([0.5, normalized_epsilon, 10*self.epsilon])
-        # self.classifier = BaseClassifier(self.epsilon, normalizer_mode=self.normalizer_mode,
-        #                                  feature_drop_probability=self.feature_drop_probability)
-        self.classifier = BaseClassifier(normalized_epsilon, normalizer_mode=self.normalizer_mode,
-                                         feature_drop_probability=self.feature_drop_probability)
+        self.classifier = BaseClassifier(normalized_epsilon)
         self.classifier.approximate_solver(self.data, self.labels, normalized_data_dist)
         self.gain = self.weight*(self.classifier.get_ginni(self.m) - self.classifier.g)
 
@@ -44,10 +39,8 @@ class TreeNode:
         pl1 = self.classifier.get_probabilities(self.data)
         pl0 = np.subtract(1.0, pl1)
         self.is_leaf = False
-        self.left = TreeNode(self.data, np.multiply(self.data_dist, pl1), self.labels, self.epsilon,
-                             self.normalizer_mode, self.feature_drop_probability)
-        self.right = TreeNode(self.data, np.multiply(self.data_dist, pl0), self.labels, self.epsilon,
-                              self.normalizer_mode, self.feature_drop_probability)
+        self.left = TreeNode(self.data, np.multiply(self.data_dist, pl1), self.labels, self.epsilon)
+        self.right = TreeNode(self.data, np.multiply(self.data_dist, pl0), self.labels, self.epsilon)
         return self.left, self.right
 
     def get_gain(self):
@@ -95,39 +88,49 @@ class TreeNode:
 
 class TreeClassifier(BaseEstimator, ClassifierMixin):
 
-    def __init__(self, epsilon=0.001, number_of_iterations=5, normalizer_mode=None, feature_drop_probability=0.0):
+    def __init__(self, epsilon=0.001, number_of_iterations=5, normalizer_mode=None, print_debug=True):
         # hyper parameters
         self.epsilon = epsilon
         self.number_of_iterations = number_of_iterations
-        self.normalizer_mode = normalizer_mode
-        self.feature_drop_probability = feature_drop_probability
+        if normalizer_mode is None or normalizer_mode == "range":
+            self.normalizer = RangeNormalizer()
+        else:
+            self.normalizer = NormOneNormalizer()
+        self.print_debug = print_debug
 
         # model
         self.root = None
 
     def fit(self, data, labels):
-        # return self.fit1(data,labels)
-        return self.fit2(data,labels)
+        norm_data = self.normalizer.normalize_train(data)
+        # return self.fit1(norm_data, labels)
+        return self.fit2(norm_data, labels)
 
     def fit1(self, data, labels):
         # initial data distribution
         data_dist = np.ones(labels.shape) / data.shape[0]
-        root = TreeNode(data, data_dist, labels, self.epsilon, self.normalizer_mode, self.feature_drop_probability)
+        root = TreeNode(data, data_dist, labels, self.epsilon)
         leaves = [root]
         for t in range(0, self.number_of_iterations):
-            print "iteration {} from {}".format(t, self.number_of_iterations)
+            if self.print_debug:
+                print "iteration {} from {}".format(t, self.number_of_iterations)
             # get maximal gain (weighted)
             gains = [l.get_gain() for l in leaves]
             if np.less_equal(np.max(gains), 0.0):
-                print 'negative gain aborting, gains:'
-                print gains
+                if self.print_debug:
+                    print 'negative gain aborting, gains:'
+                if self.print_debug:
+                    print gains
                 break
             leaf_index = np.argmax(gains)
-            print "splitting leaf with weight {} and purity {}".format(leaves[leaf_index].weight, leaves[leaf_index].m)
+            if self.print_debug:
+                print "splitting leaf with weight {} and purity {}".format(leaves[leaf_index].weight, leaves[leaf_index].m)
             # set that leaf as internal and get the split
             l, r = leaves[leaf_index].set_as_internal()
-            print "l_child weight {} and purity {}".format(l.weight, l.m)
-            print "r_child weight {} and purity {}".format(r.weight, r.m)
+            if self.print_debug:
+                print "l_child weight {} and purity {}".format(l.weight, l.m)
+            if self.print_debug:
+                print "r_child weight {} and purity {}".format(r.weight, r.m)
             # remove now-internal-node from list of leaves
             leaves.pop(leaf_index)
             # add the newly discovered leaves as potential splits
@@ -136,27 +139,31 @@ class TreeClassifier(BaseEstimator, ClassifierMixin):
 
     def fit2(self, data, labels):
         data_dist = np.ones(labels.shape) / data.shape[0]
-        root = TreeNode(data, data_dist, labels, self.epsilon, self.normalizer_mode, self.feature_drop_probability)
+        root = TreeNode(data, data_dist, labels, self.epsilon)
         self.recursive_fit(self.number_of_iterations, root)
         self.root = root
 
     def recursive_fit(self, levels, tree_node):
         if tree_node.weight < (1.0 / tree_node.data.shape[0]) or levels < 2:
-            print "stopping level {}".format(levels)
+            if self.print_debug:
+                print "stopping level {}".format(levels)
             return
-        print "splitting leaf with weight {} and purity {}".format(tree_node.weight, tree_node.m)
+        if self.print_debug:
+            print "splitting leaf with weight {} and purity {}".format(tree_node.weight, tree_node.m)
         tree_node.get_gain()
         l, r = tree_node.set_as_internal()
-        print "l_child weight {} and purity {}".format(l.weight, l.m)
-        print "r_child weight {} and purity {}".format(r.weight, r.m)
+        if self.print_debug:
+            print "l_child weight {} and purity {}".format(l.weight, l.m)
+        if self.print_debug:
+            print "r_child weight {} and purity {}".format(r.weight, r.m)
         self.recursive_fit(levels-1, l)
         self.recursive_fit(levels-1, r)
 
     def predict_deterministic(self, data, depth=-1):
-        return self.root.predict_deterministic(data, depth)
+        return self.root.predict_deterministic(self.normalizer.normalize_test(data), depth)
 
     def predict_stochastic(self, data, depth=-1):
-        return self.root.predict_stochastic(data, depth)
+        return self.root.predict_stochastic(self.normalizer.normalize_test(data), depth)
 
     def predict(self, data, y=None):
         return self.predict_deterministic(data)
@@ -217,7 +224,7 @@ if __name__ == "__main__":
     # X, y = create_data_simulation_middle()
     X, y = create_data_simulation_xor()
     # X, y = create_sized_data()
-    treeClassifier = TreeClassifier(0.05, 15, normalizer_mode="norm", feature_drop_probability=0.0)
+    treeClassifier = TreeClassifier(0.05, 15, normalizer_mode="norm", print_debug=False)
     # treeClassifier = TreeClassifier(0.05, 10, normalizer_mode="range", feature_drop_probability=0.0)
     treeClassifier.fit(X, y)
 

mnist.py
@@ -5,12 +5,30 @@ import numpy as np
 
 datasets = ['mnist', 'iris', 'breast_cancer', 'polish_companies_y1', 'polish_companies_y2', #5
             'polish_companies_y3', 'polish_companies_y4', 'polish_companies_y5', 'diabetic_retinopathy_debrecen', #9
-            'adult']
-current_dataset_index = 9
+            'adult', 'dorothea']
+current_dataset_index = 10
 depth_of_tree = 5
 
 dataset = datasets[current_dataset_index]
 
+def load_dorothea():
+    with open(r"C:\temp\thesis_data\dorothea\dorothea_train.data") as f:
+        content = f.readlines()
+    res = []
+    ps = []
+    for i in range(len(content)):
+        parts = content[i].split()
+        for p in parts:
+            if len(p)>0:
+                ps.append(int(p))
+                res.append((i-1, int(p)-1))
+    data = np.zeros([len(content),max(ps)])
+    for r in res:
+        data[r]=1.0
+    with open(r"C:\temp\thesis_data\dorothea\dorothea_train.labels") as f:
+        content = f.readlines()
+    target = np.array([1 if int(l) == 1 else 0 for l in content])
+    return data,target
 
 def load_arff(filepath):
     from scipy.io.arff import loadarff
@@ -30,6 +48,7 @@ def load_polish(year):
 def load_adult():
     import pandas as pd
     from sklearn.feature_extraction import DictVectorizer
+
     def encode_onehot(df, cols):
         vec = DictVectorizer()
 
@@ -76,6 +95,9 @@ def get_data(dataset):
     if current_dataset_index == 9:
         # http://archive.ics.uci.edu/ml/datasets/Adult
         data,target = load_adult()
+    if current_dataset_index == 10:
+        # http://archive.ics.uci.edu/ml/datasets/Dorothea
+        data,target = load_dorothea()
     return data, target
 
 data, target = get_data(dataset)